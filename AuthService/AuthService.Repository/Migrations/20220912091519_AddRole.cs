df8bed1edc5ad3aeed2ead182afb958b41d4f916
Eva Janković <evaj.evadesign@gmail.com>
2022-09-12 13:37:29 +0200

    feat: add role and profile id to user

---
+++ AuthService/AuthService.Repository/Migrations/20220912091519_AddRole.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace AuthService.Repository.Migrations
+{
+    public partial class AddRole : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Role",
+                schema: "auth",
+                table: "Users",
+                type: "text",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Role",
+                schema: "auth",
+                table: "Users");
+        }
+    }
+}
